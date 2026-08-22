1cb46c4a0e786eec82933cc8d0b08709a3d9c9c1
olivierfalahi <olivier.falahi@gmail.com>
2026-08-22 12:35:14 +0200

    feat(cli): add query parsing and improve stdin handling
    - Integrated `QueryParser` for parsing queries from CLI.
    - Improved error messages for missing query or input file.
    - Refactored stdin handling into a dedicated `read_stdin` function.

--- minijq/src/main.rs
+++ minijq/src/main.rs
@@ -2,34 +2,51 @@ mod parser;
 mod value;
 
 use crate::parser::Parser;
+use crate::query::parser::QueryParser;
 use std::io::IsTerminal;
 use std::io::Read;
 
+mod query;
 #[cfg(test)]
 mod tests;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let mut args = std::env::args();
 
-    let input = match args.nth(1) {
-        Some(path) => std::fs::read_to_string(path)?,
+    let query = match args.nth(1) {
+        Some(query) => query,
         None if std::io::stdin().is_terminal() => {
-            eprintln!("No file or stdin. Try: mini-jq <file>");
+            eprintln!("No query. Try: minijq <query> <file>");
             std::process::exit(1)
         }
-        None => {
-            let mut buffer = String::new();
-            std::io::stdin().read_to_string(&mut buffer)?;
-            if buffer.is_empty() {
-                eprintln!("Content empty. Try: mini-jq <file> or via stdin: echo '...' | mini-jq");
-                std::process::exit(1)
-            } else {
-                buffer
-            }
+        None => ".".to_string(),
+    };
+
+    let input = match args.next() {
+        Some(path) => std::fs::read_to_string(path)?,
+        None if std::io::stdin().is_terminal() => {
+            eprintln!("No file or stdin. Try: minijq . <file>");
+            std::process::exit(1)
         }
+        None => read_stdin()?,
     };
     let mut parser = Parser::new(&input);
+    let mut query_parser = QueryParser::new(&query);
     let value = parser.parse()?;
+    let query = query_parser.parse()?;
     println!("{}", value);
     Ok(())
+}
+
+fn read_stdin() -> Result<String, Box<dyn std::error::Error>> {
+    let mut buffer = String::new();
+    std::io::stdin().read_to_string(&mut buffer)?;
+    if buffer.is_empty() {
+        eprintln!(
+            "Content empty. Try: minijq <query> <file> or via stdin: echo '...' | minijq <query>"
+        );
+        std::process::exit(1)
+    } else {
+        Ok(buffer)
+    }
 }
